20bf3f934e58760dde8f50ccf0a0ca083b0a3778
swalter <filmacker@gmail.com>
2015-11-20 23:36:25 +0300

    rename file

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,7 +5,7 @@ import utils.HibernateUtil;
  * Created by Dmitriy on 20.11.2015.
  * Project TestWebApp.
  */
-public class Main {
+public class Test {
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
     }
